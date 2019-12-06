525fb596328faeea729ba93755a5df8273b4e55c
MinexAutomation <minex.automation@gmail.com>
2019-12-05 17:16:19 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Polidea.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Polidea.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
